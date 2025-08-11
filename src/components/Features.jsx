f9e3437214551eaf3a3dde529a44e1d743b63d53
Bhavith Shetty <bhavithshetty2203@gmail.com>
2025-08-11 19:45:47 +0530

    helloworld

---
+++ src/components/Features.jsx
@@ -0,0 +1,14 @@
+import "./Features.css";
+
+export default function Features() {
+  return (
+    <>
+      <div className="feature-container">
+        <div className="features-body">
+          <p className="feature-text2">Feature</p>
+          <h1 className="feature-text1">Features</h1>
+        </div>
+      </div>
+    </>
+  );
+}
